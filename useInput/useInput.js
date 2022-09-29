5a987182facde04eba3e57891da054b8e7bad9d4
Seolang <dntmdqls0912@gmail.com>
2022-09-29 17:57:52 +0900

    more more hooks

--- useInput/useInput.js
+++ useInput/useInput.js
@@ -21,3 +21,14 @@ export const useInput = (initialValue, validator) => {
 
   return { value, onChange };
 };
+
+// export default function App() {
+//   const maxLen = value => !value.includes("@");
+//   const name = useInput("Mr. ", maxLen);
+//   return (
+//     <div className="App">
+//       <h1>Hello</h1>
+//       <input placeholder="Name" {...name} />
+//     </div>
+//   );
+// }
